2b0af81823a28d96fff24a4f859b61c50fb730b6
Harikumar4 <104781897+Harikumar4@users.noreply.github.com>
2025-07-09 16:34:05 +0530

    [refactor]: optimise pdf parsing within single pass

--- backend/utils/pdf_extractor.py
+++ backend/utils/pdf_extractor.py
@@ -4,32 +4,40 @@ import io
 import pdfplumber
 import pandas as pd
 
-def extract_text_pdf(file):
-    doc = fitz.open(stream=file.read(), filetype="pdf")
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    return text
 
-def extract_images_pdf(file):
-    doc = fitz.open(stream=file.read(), filetype="pdf")
-    images = []
-    for page_index in range(len(doc)):
-        for img_index, img in enumerate(doc[page_index].get_images(full=True)):
+def embed_pages(file):
+    chunks = []
+    file.seek(0)
+    fitz_doc = fitz.open(stream=file.read(), filetype="pdf")
+    file.seek(0)
+    plumber_pdf = pdfplumber.open(file)
+    total_pages = len(fitz_doc)
+    for i in range(total_pages):
+        text = fitz_doc[i].get_text().strip()
+        chunks.append({"type": "text", "content": text, "page": i})
+        images = fitz_doc[i].get_images(full=True)
+        for idx,img in enumerate(images):
             xref = img[0]
-            base_image = doc.extract_image(xref)
+            base_image = fitz_doc.extract_image(xref)
             image_bytes = base_image["image"]
             img_pil = Image.open(io.BytesIO(image_bytes))
-            images.append(img_pil)
-    return images
-
-def extract_tables_pdf(file):
-    tables = []
-    with pdfplumber.open(file) as pdf:
-        for page in pdf.pages:
-            extracted = page.extract_tables()
-            for table in extracted:
+            chunks.append({
+                "type": "image",
+                "content": img_pil,
+                "page": i,
+                "position": idx
+            })
+        if i < len(plumber_pdf.pages):
+            tables = plumber_pdf.pages[i].extract_tables()
+            for table in tables:
                 df = pd.DataFrame(table)
                 if df.shape[1] >= 2 and df.shape[0] > 1:
-                    tables.append(df)
-    return tables
+                    chunks.append({
+                        "type": "table",
+                        "content": df.to_csv(index=False),
+                        "page": i
+                    })
+    fitz_doc.close()
+    plumber_pdf.close()
+
+    return chunks
